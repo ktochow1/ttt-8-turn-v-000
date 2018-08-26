7515769b0b19b4d3585af558ecd84307b2fadade
ktochow1 <ktochow1@binghamton.edu>
2018-08-26 00:00:49 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def move(board, index, token="X")
   board[index] = token
 end
 
-def turn(board)
+def turn(board, index)
   puts "Please enter 1-9:"
   user_input = gets
   input_to_index(user_input)
